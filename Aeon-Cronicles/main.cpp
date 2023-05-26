c32d292e8cf9d83bc79aa71e1b2192bd533d04f1
TFaron1 <tomf1911@hotmail.com>
2023-05-26 11:46:45 -0400

    window created

--- Aeon-Cronicles/main.cpp
+++ Aeon-Cronicles/main.cpp
@@ -2,6 +2,18 @@
 #include "SDL.h"
 int main(int argc, char* argv[])
 {
+	bool gameRunning = true;
+
+	SDL_Window* pWindow;
+
+	pWindow = SDL_CreateWindow("Aeon Cronicles", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1080, 780,0);
+
+	
+
+	while (gameRunning == true)
+	{
+
+	}
 	std::cout << "world" << std::endl;
 	return 0;
 }
