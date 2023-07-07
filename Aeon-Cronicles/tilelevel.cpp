48600ba57afa2d19ae1d8614bcc4c36c4ffa8907
lloonnggg <hohoanglong0505@gmail.com>
2023-07-07 00:18:08 -0400

    update 2

--- Aeon-Cronicles/tilelevel.cpp
+++ Aeon-Cronicles/tilelevel.cpp
@@ -1,33 +1,96 @@
 #include "tilelevel.h"
+#include <fstream>
 
-TileLevel::TileLevel() {
+TileLevel::TileLevel()
+{
     tileSize = 32;  // Adjust this value according to your tile size
 }
 
-TileLevel::~TileLevel() {}
-
-bool TileLevel::loadLevel(const std::string& filename, SDL_Renderer* renderer) {
-    // Load the tile level from the file
-    // Parse the file and populate the "tiles" vector with tile IDs
-    // Return true if loading is successful, false otherwise
+TileLevel::~TileLevel()
+{
+    for (SDL_Texture* texture : tileTextures)
+    {
+        SDL_DestroyTexture(texture);
+    }
 }
 
-void TileLevel::render(SDL_Renderer* renderer) {
-    for (int row = 0; row < tiles.size(); ++row) {
-        for (int col = 0; col < tiles[row].size(); ++col) {
-            int tileID = tiles[row][col];
+bool TileLevel::loadLevel(SDL_Renderer* renderer)
+{
+    std::ifstream file("TileData.txt");
+    if (!file.is_open())
+    {
+        return false;  // Unable to open the file
+    }
+    else
+    {
+        std::cout << "Reading data file";
+    }
 
-            // Calculate the position of the current tile
+    std::vector<int> row;
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+        row.clear();
+        for (char tileID : line)
+        {
+            row.push_back(tileID - '0');
+        }
+        tiles.push_back(row);
+    }
+    file.close();
+
+    // Load tile textures based on tile IDs
+    for (int row = 0; row < tiles.size(); ++row)
+    {
+        for (int col = 0; col < tiles[row].size(); ++col)
+        {
+            int tileID = tiles[row][col];
+            std::string imagePath = "tile" + std::to_string(tileID) + ".png";
+
+            // Load the image using SDL_image
+            SDL_Surface* surface = IMG_Load(imagePath.c_str());
+            if (!surface)
+            {
+                // Error loading the image, handle accordingly
+                return false;
+            }
+
+            // Create a texture from the surface
+            SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+            if (!texture)
+            {
+                // Error creating texture, handle accordingly
+                SDL_FreeSurface(surface);
+                return false;
+            }
+
+            // Add the texture to the tileTextures vector
+            tileTextures.push_back(texture);
+
+            // Free the surface as it's no longer needed
+            SDL_FreeSurface(surface);
+        }
+    }
+
+    return true;
+}
+
+void TileLevel::render(SDL_Renderer* renderer)
+{
+    for (int row = 0; row < tiles.size(); ++row)
+    {
+        for (int col = 0; col < tiles[row].size(); ++col)
+        {
+            int tileID = tiles[row][col];
+            SDL_Texture* texture = tileTextures[tileID];
+
             int x = col * tileSize;
             int y = row * tileSize;
 
-            // Render the tile based on its ID
-            // Use SDL_Rect to define the position and size of the tile on the screen
-            SDL_Rect tileRect = { x, y, tileSize, tileSize };
+            SDL_Rect destinationRect = { x, y, tileSize, tileSize };
 
-            // Example rendering code (change this based on your specific tile representation)
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  // Set the tile color to white
-            SDL_RenderFillRect(renderer, &tileRect);  // Render the tile rectangle
+            SDL_RenderCopy(renderer, texture, nullptr, &destinationRect);
         }
     }
 }
--- Aeon-Cronicles/tilelevel.h
+++ Aeon-Cronicles/tilelevel.h
@@ -3,19 +3,22 @@
 #define TILE_LEVEL_H
 
 #include <SDL.h>
+#include <SDL_image.h>
 #include <vector>
 #include <string>
+#include <iostream>
 
 class TileLevel {
 public:
     TileLevel();
     ~TileLevel();
 
-    bool loadLevel(const std::string& filename, SDL_Renderer* renderer);
+    bool loadLevel(SDL_Renderer* renderer);
     void render(SDL_Renderer* renderer);
 
 private:
     std::vector<std::vector<int>> tiles;
+    std::vector<SDL_Texture*> tileTextures;
     int tileSize;
 };
 
