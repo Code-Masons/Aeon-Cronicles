1bbc9f1dc69193ba29b5f066dd2e259abb473df2
zozotub <90784489+zozotub@users.noreply.github.com>
2023-07-07 07:58:22 -0400

    Player animation

--- Aeon-Cronicles/GameObject.cpp
+++ Aeon-Cronicles/GameObject.cpp
@@ -1,8 +1,8 @@
 #include "GameObject.h"
 
 GameObject::GameObject(float x, float y, float w, float h, Uint8 r, Uint8 g, Uint8 b, Uint8 a)
-	: m_Transform{ x , y , w , h }
-	, m_Color{ r, g, b, a }
+	: m_transform{ x , y , w , h }
+	, m_Color{ r, g, b, a }, m_enabled(true) 
 {
 
 }
@@ -10,15 +10,61 @@ void GameObject::Draw(SDL_Renderer* pRenderer)
 {
 
 	SDL_SetRenderDrawColor(pRenderer, m_Color.r, m_Color.g, m_Color.b, m_Color.a);
-	SDL_RenderFillRectF(pRenderer, &m_Transform);
+	SDL_RenderFillRectF(pRenderer, &m_transform);
 }
+SDL_FRect* GameObject::GetTransform()
+{
+	return &m_transform;
+}
+SDL_FPoint GameObject::GetCenter() const
+{
+	return { m_transform.x + m_transform.w / 2, m_transform.y + m_transform.h / 2 };
+}
+const bool GameObject::GetEnabled() const
+{
+	return m_enabled;
+}
+void GameObject::SetEnabled(const bool e)
+{
+	m_enabled = e;
+}
+Sprite::Sprite(const SDL_Rect src, const SDL_FRect transform)
+	:GameObject(transform), m_src(src) {}
+
+SDL_Rect* Sprite::GetSrc()
+{
+	return &m_src;
+}
+void AnimatedSprite::SetAnimation(AnimState state, const unsigned short frameMax, const unsigned short spriteMin,
+	const unsigned short spriteMax, const int srcY)
+{
+	m_state = state;
+	m_frame = 0;
+	m_frameMax = frameMax;
+	m_sprite = m_spriteMin = spriteMin;
+	m_spriteMax = spriteMax;
+	m_src.x = m_src.w * m_sprite;
+	m_src.y = srcY;
+}
+void AnimatedSprite::Animate()
+{
+	if (m_frame++ == m_frameMax)
+	{
+		m_frame = 0;
+		if (++m_sprite == m_spriteMax)
+			m_sprite = m_spriteMin; // I don't use % in case m_sprite doesn't start at 0.
+		m_src.x = m_src.w * m_sprite; // Moved to outside if.
+	}
+}
+
 
 void GameObject::UpdatePositionX(float x)
 {
-	m_Transform.x += x;
+	m_transform.x += x;
 }
 void GameObject::UpdatePositionY(float y)
 {
-	m_Transform.y += y;
+	m_transform.y += y;
 }
+
 ////Men
--- Aeon-Cronicles/GameObject.h
+++ Aeon-Cronicles/GameObject.h
@@ -1,21 +1,55 @@
 #pragma once
 #include <SDL.h>
 
+enum AnimState { STATE_IDLING, STATE_RUNNING, STATE_JUMPING };
+
 class GameObject
 {
 public:
 	GameObject(float x, float y, float w, float h, Uint8 r = 255, Uint8 g = 255, Uint8 b = 255, Uint8 a = 255);
 
 	void Draw(SDL_Renderer* pRenderer);
+	//SDL_FRect* GetTransform();//void UpdatePositionX(float x);
+	SDL_FPoint GetCenter() const;
+	
 	void UpdatePositionX(float x);
 	void UpdatePositionY(float y);
 
-	SDL_FRect& GetTransform()
-	{
-		return m_Transform;
-	}
-private:
-	SDL_FRect m_Transform;
+	SDL_FRect* GetTransform();
+	virtual void Update() = 0;
+	virtual void Render() = 0;
+	const bool GetEnabled() const;
+	void SetEnabled(const bool e);
+protected:
+	SDL_FRect m_transform;
 	SDL_Color m_Color;
+	bool m_enabled;
+	GameObject();
+	GameObject(const SDL_FRect transform);
+};
+class Sprite : public GameObject
+{
+public:
+	SDL_Rect* GetSrc();
+	virtual void Update() = 0;
+	virtual void Render() = 0;
+
+protected:
+	SDL_Rect m_src;
+	Sprite(const SDL_Rect src, const SDL_FRect transform);
+};
+class AnimatedSprite : public Sprite
+{
+public:
+	virtual void Update() = 0;
+	virtual void Render() = 0;
+
+protected:
+	unsigned short m_frame, m_frameMax, m_sprite, m_spriteMin, m_spriteMax;
+	AnimState m_state;
+	AnimatedSprite(const SDL_Rect src, const SDL_FRect transform, AnimState state);
+	void SetAnimation(AnimState state, const unsigned short frameMax, const unsigned short spriteMin,
+		const unsigned short spriteMax, const int srcY = 0);
+	void Animate();
 };
 
